c027a94f133f7f610711a39c03392221c71dcf39
Camellia Fleming <94232095+camelliafleming@users.noreply.github.com>
2023-02-06 21:12:27 -0500

    Update flemingc09_prog2.py

--- flemingc09_prog2.py
+++ flemingc09_prog2.py
@@ -2,8 +2,7 @@
 Author:  Camellia Fleming
 Course:  CSC 121
 Assignment:  Lab Lesson 09 - Part 2
-Description:  Generate 20 random numbers between 1 and 100, inclusive,
-and store them in a list
+Description:  Generate 20 random numbers between 1 and 100, inclusive, and store them in a list
 """
 import random
 
